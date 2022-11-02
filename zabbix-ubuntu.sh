1ba205aebf79e54dac0fc25310e2257b9698c903
taher <ahmad.taher@arabyads.com>
2022-11-02 19:45:05 +0300

    Project Init

--- zabbix-ubuntu.sh
+++ zabbix-ubuntu.sh
@@ -5,7 +5,8 @@ sudo apt-get update
 sudo apt-get install zabbix-agent -y
 sed -i 's/ServerActive=127.0.0.1/ServerActive=YOUR_SERVER_NAME,YOUR_SERVER_IP/g' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/Server=127.0.0.1/Server=YOUR_SERVER_NAME,YOUR_SERVER_IP/g' /etc/zabbix/zabbix_agentd.conf
+sudo ufw allow 10050/tcp
 sudo systemctl restart zabbix-agent
 sudo systemctl status zabbix-agent
-sudo ufw allow 10050/tcp
+
 
